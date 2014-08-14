6105b2461feaf6a3c1cd61245d0baa1081677094
shinyorke <spirits.is.my.rader@gmail.com>
2014-08-14 17:41:19 +0900

    Create database and user(admin, app).

--- chef-solo/site-cookbooks/migrate/recipes/default.rb
+++ chef-solo/site-cookbooks/migrate/recipes/default.rb
@@ -12,7 +12,28 @@ mysql_connection_info = {:host => "localhost",
                          :username => 'root',
                          :password => node['mysql']['server_root_password']}
 
-mysql_database "sean_lahman" do
+
+# create database
+mysql_database node['database']['name'] do
   connection mysql_connection_info
   action :create
+end
+
+# create & grant admin username
+mysql_database_user node['database']['user']['admin']['name'] do
+  connection mysql_connection_info
+  password node['database']['user']['admin']['password']
+  database_name node['database']['name']
+  privileges [:all]
+  action [:create, :grant]
+end
+
+
+# create & grant application user
+mysql_database_user node['database']['user']['app']['name'] do
+  connection mysql_connection_info
+  password node['database']['user']['app']['password']
+  database_name node['database']['name']
+  privileges [:select, :update, :insert, :delete]
+  action [:create, :grant]
 end
